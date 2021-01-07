094aa0977604e0b691e1dea67cbe06f5286d5fcc
alex <alex@susanu.com>
2021-01-07 17:09:19 +0000

    added char

--- assignments/assignment5/src/com/coderscampus/arraylist/CustomListApplication.java
+++ assignments/assignment5/src/com/coderscampus/arraylist/CustomListApplication.java
@@ -2,9 +2,14 @@ package com.coderscampus.arraylist;
 
 public class CustomListApplication {
     public static void main(String[] args){
-        CustomList<Integer> customList = new CustomArrayList<>();
-        for (int i = 0; i < 101; i++){
-            customList.add(i);
+        CustomList<Character> customList = new CustomArrayList<>();
+        char j = 'a';
+        for (int i = 0; i < 26; i++){
+            customList.add(j);
+            j++;
+        }
+
+        for (int i = 0; i < 26; i++){
             System.out.println(customList.get(i));
         }
     }
