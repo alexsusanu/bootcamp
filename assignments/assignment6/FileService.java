62c70ad00fa71854edea64a90c6678973464a52f
alex <alex@susanu.com>
2021-01-17 20:07:30 +0000

    1st draft

---
+++ assignments/assignment6/FileService.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Stream;
+
+public class FileService {
+    public FileService(){};
+
+    /** output report based on filename
+     * @param file use filename for output
+     * capitalize filename, split to get the model name/number
+     * output with best month/worst month string
+     */
+    public void outputReport(File file) {
+        String fileWithoutExtension = file.getName().substring(0,6);
+        String model = fileWithoutExtension.substring(0,5);
+        String modelName = fileWithoutExtension.substring(5,6);
+        String tesla = capitalize(model) + " " + modelName;
+
+        System.out.println("The best month for " + tesla + " was:");
+        System.out.println("The worst month for " + tesla + " was:");
+    }
+
+    /** capitalize first letter
+     * @param arg name to capitalize
+     */
+    public String capitalize(String arg){
+        return arg.substring(0,1).toUpperCase() + arg.substring(1);
+    }
+
+    /** get sales date from either min or max value as String
+     * @param file to get data from, csv type
+     * @param value -> either min or max value
+     * @return date for the min or max value
+     */
+    public String getSalesDate(File file, Integer value) {
+        String line;
+        String result = null;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader.readLine();
+            while((line = reader.readLine()) != null){
+                if(Integer.parseInt(line.split(",")[1]) == value){
+                    result = line.split(",")[0];
+                }
+            }
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /** get statistic information
+     * @param file of type csv to read data from
+     * @param minMax -> minMax == "min" -> get min value from file
+     * @param minMax -> minMax == "max" -> get max value from file
+     * @return min or max value
+     */
+    public Integer getStatisticsInfo(File file, String minMax) {
+        IntSummaryStatistics valueSales = null;
+        Integer result = null;
+        try(Stream<String> lines = Files.lines(file.toPath())){
+            valueSales = lines.map(line -> line.split(","))
+                    .skip(1)
+                    .mapToInt(line -> Integer.parseInt(line[1]))
+                    .summaryStatistics();
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (minMax.equals("min")){
+            result = valueSales.getMin();
+        }else if (minMax.equals("max")){
+            result = valueSales.getMax();
+        }
+        return result;
+    }
+}
---
+++ assignments/assignment6/TeslaAnalysis.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Stream;
+import java.io.FileReader;
+
+public class TeslaAnalysis {
+    public static void main(String[] args) {
+        File file = new File("modelS.csv");
+        FileService fileService = new FileService();
+    }
+
+
+}
