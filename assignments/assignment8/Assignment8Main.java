6d892158170927c44ec732052b897f8492a7615c
alex <alex@susanu.com>
2021-02-04 20:38:08 +0000

    2nd draft

--- assignments/assignment8/Assignment8Main.java
+++ assignments/assignment8/Assignment8Main.java
@@ -1,6 +1,10 @@
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Assignment8Main{
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             Run run = new Run();
             new Thread(run).start();
         }
--- assignments/assignment8/Run.java
+++ assignments/assignment8/Run.java
@@ -4,7 +4,9 @@ public class Run implements  Runnable{
     @Override
     public void run() {
         Assignment8 assignment8 = new Assignment8();
-        List<Integer> numberList = assignment8.getNumbers();
-        System.out.println(numberList);
+        for (int i = 0; i < 1000; i++) {
+            List<Integer> numberList = assignment8.getNumbers();
+            System.out.println(numberList);
+        }
     }
 }
