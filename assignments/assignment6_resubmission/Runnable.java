0c6d08a375a2d46e8a60d07bb64bc6c0b43d5803
alex <alex@susanu.com>
2021-06-16 20:27:22 +0100

    small updates

--- assignments/assignment6_resubmission/Runnable.java
+++ assignments/assignment6_resubmission/Runnable.java
@@ -1,16 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
-import java.util.Map;
 
 public class Runnable {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
         TeslaModelsService teslaModelsService = new TeslaModelsService();
         FileService fileService = new FileService();
         for(int i = 0; i < args.length; i++) {
             File file = new File(args[i]);
             List<TeslaModels> teslaModelsList = teslaModelsService.addModelInfo(fileService.readFile(file));
-            Map<String, Map<String, String>> map = teslaModelsService.getSummaryStatistics(teslaModelsList, "min");
             teslaModelsService.fullReport(teslaModelsList, file, fileService);
         }
     }
