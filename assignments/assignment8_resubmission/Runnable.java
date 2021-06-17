047b8dd0f2ee3ac380df6573794fd4c648801069
alex <alex@susanu.com>
2021-06-17 10:02:29 +0100

    testing

--- assignments/assignment8_resubmission/Runnable.java
+++ assignments/assignment8_resubmission/Runnable.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Runnable {
+import com.coderscampus.assignment.Assignment8;
+
+import java.util.List;
+
+public class Runnable {
+    public static void main(String[] args) {
+        Assignment8 assignment8 = new Assignment8();
+        for (int i=0; i<1000; i++) {
+
+            List<Integer> numbersList = assignment8.getNumbers();
+
+            System.out.println(numbersList);
+
+        }
+
+
+    }
 }
